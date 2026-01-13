97360e0cb3e9717294ebc65dceb149a9d8aa13d3
quantum0059 <shivamsharma.anva@gmail.com>
2026-01-13 14:58:00 +0530

    Add Day1 Spring Boot project

---
+++ Day1/src/main/java/com/example/Day1/hello.java
@@ -0,0 +1,14 @@
+package com.example.Day1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class hello {
+
+    @GetMapping("/")
+    public String hello(){
+        return "This is the first time";
+    }
+}
