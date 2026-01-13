87483310317084547a9d036e9a9c5f6692e2a329
quantum0059 <shivamsharma.anva@gmail.com>
2026-01-13 15:06:46 +0530

    Adding additional route

--- Day1/src/main/java/com/example/Day1/hello.java
+++ Day1/src/main/java/com/example/Day1/hello.java
@@ -10,5 +10,16 @@ public class hello {
     @GetMapping("/")
     public String hello(){
         return "This is the first time";
+
+    }
+
+    @GetMapping("/home")
+    public String home(){
+        return "This is Home page";
+    }
+
+    @GetMapping("/class")
+    public String cls(){
+        return "This is a new class";
     }
 }
